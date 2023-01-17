2d834a56d05c2474a00b7ce539c9d59a3e990b8e
vaibhav shitole <116602594+VaibhavShitole1303@users.noreply.github.com>
2023-01-17 19:00:55 +0545

    Initial commit

---
+++ app/src/main/java/com/example/whatsappclone/Call_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.whatsappclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Call_Fragment extends Fragment {
+
+
+    public Call_Fragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_call_, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+     TabLayout tabLayout;
+     ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager=findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        tabLayout=findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
